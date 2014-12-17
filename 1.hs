4f846558bfb149bde39e198357199bf6b98f9d36
Maxim Kalachev <maxkalachev@yandex.ru>
2014-12-17 21:24:00 +0300

    problem 2

--- 1.hs
+++ 1.hs
@@ -1,1 +1,3 @@
-sum [x| x <- [3..999], x `mod` 3 == 0 || x `mod` 5 == 0]
+
+s = sum [x| x <- [3..999], x `mod` 3 == 0 || x `mod` 5 == 0]
+main = print(s)
---
+++ 2.hs
@@ -0,0 +1,6 @@
+
+
+s = sum [x | x <- takeWhile (<= 4000000) fibs, even x]
+  where fibs = 1 : 1 : (zipWith (+) fibs (tail fibs))
+
+main = print(s)
