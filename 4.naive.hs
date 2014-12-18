7fb4f45e4c36d9d9f5b45af63c8064f3c7b82f99
Maxim Kalachev <maxkalachev@yandex.ru>
2014-12-18 20:19:58 +0300

    problem 4

--- 4.naive.hs
+++ 4.naive.hs
@@ -1,8 +1,4 @@
 
--- A palindromic number reads the same both ways.
--- The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 × 99.
--- Find the largest palindrome made from the product of two 3-digit numbers.
-
 digits :: Integer -> [Integer]
 digits 0 = []
 digits x = digits (x `div` 10) ++ [x `mod` 10]
