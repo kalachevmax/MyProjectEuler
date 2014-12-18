a3425510ba7d4337716c3a83b9458c68eb34511c
Maxim Kalachev <maxkalachev@yandex.ru>
2014-12-18 21:37:44 +0300

    gcd and lcm

--- 5.lcm.hs
+++ 5.lcm.hs
@@ -1,9 +1,10 @@
 
-
+-- greater common divisor
 gcd1 :: Int -> Int -> Int
 gcd1 a 0 = a
 gcd1 a b = gcd b (a `mod` b)
 
+-- least common multiple
 lcm1 :: Int -> Int -> Int
 lcm1 a b = abs(a * b) `div` gcd1 a b
 
